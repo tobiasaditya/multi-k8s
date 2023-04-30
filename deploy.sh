266fcf5ab676a32941a621cdbaa59522f0a6a959
Tobias <tobias.aditya@gmail.com>
2023-04-30 20:18:50 +0700

    fix script set image by tag

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t tobiasaditya/multi-client:latest -t tobiasaditya/multi-client:$SHA ./client/Dockerfile ./client
 docker build -t tobiasaditya/multi-server:latest -t tobiasaditya/multi-server:$SHA ./server/Dockerfile ./server
 docker build -t tobiasaditya/react-worker:latest -t tobiasaditya/multi-worker:$SHA ./worker/Dockerfile ./worker
+
 docker push tobiasaditya/multi-client:latest
 docker push tobiasaditya/multi-server:latest
 docker push tobiasaditya/multi-worker:latest
@@ -10,6 +11,6 @@ docker push tobiasaditya/multi-server:$SHA
 docker push tobiasaditya/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=docker_id/multi-client:$SHA
-kubectl set image deployments/server-deployment server=docker_id/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=docker_id/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=tobiasaditya/multi-client:$SHA
+kubectl set image deployments/server-deployment server=tobiasaditya/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=tobiasaditya/multi-worker:$SHA
